0ba8019ad1bfce42a7d948f15d3fdba24506efd1
Gaurang <contactgaurangshirodkar@gmail.com>
2026-09-02 04:21:45 +0000

    Update instrument test to add more tests

--- sprint05/Sprint05-work/src/test/java/com/tradingsystem/domain/entities/InstrumentTest.java
+++ sprint05/Sprint05-work/src/test/java/com/tradingsystem/domain/entities/InstrumentTest.java
@@ -89,6 +89,99 @@ class InstrumentTest {
         assertFalse(instrument.mayBeTraded());
     }
 
+    @Test
+    void shouldRejectBlankSymbol() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Instrument(
+                        "",
+                        "Tata Consultancy Services",
+                        AssetClass.EQUITY,
+                        "INR"
+                )
+        );
+    }
+
+    @Test
+    void shouldRejectBlankDisplayName() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Instrument(
+                        "TCS",
+                        "",
+                        AssetClass.EQUITY,
+                        "INR"
+                )
+        );
+    }
+
+    @Test
+    void shouldRejectNullAssetClass() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Instrument(
+                        "TCS",
+                        "Tata Consultancy Services",
+                        null,
+                        "INR"
+                )
+        );
+    }
+
+    @Test
+    void shouldRejectBlankQuotationCurrency() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Instrument(
+                        "TCS",
+                        "Tata Consultancy Services",
+                        AssetClass.EQUITY,
+                        ""
+                )
+        );
+    }
+
+    @Test
+    void shouldInitiallyBeTradable() {
+        Instrument instrument = new Instrument(
+                "TCS",
+                "Tata Consultancy Services",
+                AssetClass.EQUITY,
+                "INR"
+        );
+
+        assertTrue(instrument.mayBeTraded());
+    }
+
+    @Test
+    void shouldNotBeTradableAfterDelisting() {
+        Instrument instrument = new Instrument(
+                "TCS",
+                "Tata Consultancy Services",
+                AssetClass.EQUITY,
+                "INR"
+        );
+
+        instrument.delist();
+
+        assertFalse(instrument.mayBeTraded());
+    }
+
+    @Test
+    void delistingShouldBeIrreversible() {
+        Instrument instrument = new Instrument(
+                "TCS",
+                "Tata Consultancy Services",
+                AssetClass.EQUITY,
+                "INR"
+        );
+
+        instrument.delist();
+        instrument.delist();
+
+        assertFalse(instrument.mayBeTraded());
+    }
+
 
     private Instrument createInstrument() {
 
